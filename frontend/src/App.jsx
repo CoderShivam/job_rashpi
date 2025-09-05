6b811a4d4036390a03708c72773e4b6adb89ea21
CoderShivam <shivamkumargupta3456@gmail.com>
2025-09-05 06:53:46 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Auth/Login.jsx";
 import Arena from "./components/Pages/Arena.jsx";
 import AppRoutes from "./routes.jsx";
 import { Toaster } from "react-hot-toast";
 import Home from "./components/Pages/home.jsx";
-import ProtectedRoute from "./components/PrivateRoute.jsx";
 import QuestionsPage from "./components/Pages/Platforms/Questions.jsx";
 import PlatformList from "./components/Pages/Platforms/PlatformList.jsx";
 import ResumeBuilder from "./components/Pages/ResumeBuilder.jsx";
@@ -15,44 +14,31 @@ import ResourcesPage from "./components/Pages/Resources.jsx";
 import CompaniesPage from "./components/Pages/CompaniesPage.jsx";
 import Register from "./components/Auth/Signup.jsx";
 import Experience from "./components/Pages/Experience.jsx";
-import { useEffect } from "react";
 import SetLoginFromURL from "../Setgooglelogin.jsx";
+
 const App = () => {
-  // useEffect(() => {
-  //   const params = new URLSearchParams(window.location.search);
-  //   console.log("URL Params:", params.toString()); // Debugging
-
-  //   if (params.get("isLoggedIn") === "true") {
-  //     console.log("Setting isLoggedIn to localStorage"); // Debugging
-  //     localStorage.setItem("isLoggedIn", "true");
-
-  //     // ✅ Remove `isLoggedIn` from URL after storing it
-  //     window.history.replaceState({}, document.title, window.location.pathname);
-  //   }
-  // }, []);
-  <SetLoginFromURL />
-
   return (
-      <Router>
-        <Routes>
-        {/* Protected Arena Route */}
-        
-        <Route path="/arena" element={<ProtectedRoute element={<Arena />} />} />
-        <Route path="/questions" element={<ProtectedRoute element={<QuestionsPage/>}/>}/>
-    <Route path="/resumebuilder" element={<ProtectedRoute element={<ResumeBuilder/>}/>}/>
-    <Route path="/platformlist" element={<ProtectedRoute element={<PlatformList/>}/>}/>
-    <Route path="/platform/QuestionsPage" element={<ProtectedRoute element={<QuestionsPage />} />}/>
-    <Route path="/platform/CodeforcesAPI" element={<ProtectedRoute element={<CodeforcesAPI />} />}/>
-    <Route path="/platform/ContestsWithLinks" element={<ProtectedRoute element={<ContestsWithLinks />} />}/>
-        {/* Public Route */}
+    <Router>
+      <SetLoginFromURL />
+      <Routes>
+        {/* Arena & Platforms */}
+        <Route path="/arena" element={<Arena />} />
+        <Route path="/questions" element={<QuestionsPage />} />
+        <Route path="/resumebuilder" element={<ResumeBuilder />} />
+        <Route path="/platformlist" element={<PlatformList />} />
+        <Route path="/platform/QuestionsPage" element={<QuestionsPage />} />
+        <Route path="/platform/CodeforcesAPI" element={<CodeforcesAPI />} />
+        <Route path="/platform/ContestsWithLinks" element={<ContestsWithLinks />} />
+
+        {/* Public Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
-        <Route path="/resources" element={<ProtectedRoute element={<ResourcesPage/>}/>}/>
-        <Route path="/companiespage" element={<CompaniesPage />}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path = "/experience" element = {<Experience/>}/>
+        <Route path="/resources" element={<ResourcesPage />} />
+        <Route path="/companiespage" element={<CompaniesPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/experience" element={<Experience />} />
       </Routes>
-      </Router>
+    </Router>
   );
 };
 
